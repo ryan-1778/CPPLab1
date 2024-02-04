187b8bb0755756190779e4cdbbc509a5038533f0
Emma Ryan <122934723+ryan-1778@users.noreply.github.com>
2024-02-04 15:49:22 -0500

    Add files via upload

---
+++ temperature.cpp
@@ -0,0 +1,13 @@
+#include "temperature.h"
+
+
+double celsius2fahrenheit(double celsius) {
+    double answer = 0;
+    // Your code here
+    return answer;
+}
+
+void Temperature::fahrenheit2celsius(double fahrenheit) {
+    // Your code here
+    return answer;
+}
